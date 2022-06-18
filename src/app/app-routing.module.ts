c050725a5ee967909cebbef275135a2d60a110e9
Li-Haowei <haoweilee666@gmail.com>
2022-06-18 00:18:07 -0400

    create files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DiscoverComponent } from './discover/discover.component';
+import { ExploreComponent } from './explore/explore.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'discover', component: DiscoverComponent},
+  {path: 'explore', component: ExploreComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ExploreComponent } from './explore/explore.component';
+import { DiscoverComponent } from './discover/discover.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ExploreComponent,
+    DiscoverComponent,
   ],
   imports: [
     BrowserModule,
